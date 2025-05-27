28e75b3196639dbe3537d0e920dbc03731576191
acxe21 <jeromedacasin21@gmail.com>
2025-05-28 00:39:06 +0800

    add initialization for frontend

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Laravel + Vue Router</h1>
+    <router-link to="/home">Go to Home</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
